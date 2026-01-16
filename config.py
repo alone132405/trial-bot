ec1bb65a7901ebecf543cbb49137e0f0c5ff20d3
heloooo992 <shakyaprakhar20@gmail.com>
2026-01-16 23:58:17 +0530

    Add captured credentials to config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """
 Lords Mobile Protocol Client - Configuration
-Your actual credentials for testing
+Your actual credentials (captured via Frida!)
 """
 
 # Account Credentials
@@ -8,17 +8,20 @@ IGG_ID = 987303841
 GAME_ID = 1051029902
 DEVICE_ID = "0d48db23-1d6e-437e-8b66-c4014090c857"
 
-# Access Key - Still need to find this!
-# The acc.json contains encrypted data, not the raw access key
-# Options to get it:
-# 1. Capture with Wireshark when bot logs in
-# 2. Use HTTP Catcher on phone
-# 3. Decrypt the acc.json (need to find the encryption key)
-ACCESS_KEY = ""  # TO BE FILLED
+# Access Token (captured from network traffic!)
+# JWT format: header.signature
+ACCESS_TOKEN = "eyJha2lkIjoyMDIxMDQyMiwicmtpZCI6MjAyMTA0MjMsImciOjEwNTEwMjk5MDIsInYiOjN9.RfdfNm8R60p4wrmxVhQUOm3UEU5MYdDeswJ63yh4E6JXhI9M2d_v9hK7p"
+
+# Decoded token info:
+# akid: 20210422 (Access Key ID)
+# rkid: 20210423 (Request Key ID)
+# g: 1051029902 (Game ID)
+# v: 3 (Version)
 
 # IGG Server Endpoints
 API_ENDPOINT = "https://cgi.igg.com"
 GAME_SERVER = "game.igg.com"
 
-# Game Version (check current version)
+# Game Version
 GAME_VERSION = "5.80"
+
